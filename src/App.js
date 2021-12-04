0d76311f82f8a43915659ebf2260c302cc833bdf
Md sowrov sarkar <dev.sowrovsarkar63@gmail.com>
2021-12-05 01:05:57 +0600

    mui experimentation done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import Experiments from "./ExperimentMui/Experiments";
 import BasicNav from "./Navbars/BasicNav/BasicNav";
 
 function App() {
     return (
         <>
             <BrowserRouter>
+                <Link to="/experiment">Experiment</Link>
+                <br />
+                <br />
                 <Routes>
                     <Route path="/" element={<BasicNav />} />
+                    <Route path="/experiment" element={<Experiments />} />
                 </Routes>
             </BrowserRouter>
         </>
---
+++ src/ExperimentMui/Experiments.js
@@ -0,0 +1,14 @@
+import { Button } from "@mui/material";
+import React from "react";
+
+const Experiments = () => {
+    return (
+        <div>
+            <h2>Experimentals</h2>
+            {/* mui button  */}
+            <Button variant="contained">Experimental button </Button>
+        </div>
+    );
+};
+
+export default Experiments;
